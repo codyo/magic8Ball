c8b4116a1b603b3aad51c815101541323472e356
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-03 13:17:53 -0700

    added functionality to shuffle images and shake

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray:Array = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber:Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
     }
-
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
 
 }
 
